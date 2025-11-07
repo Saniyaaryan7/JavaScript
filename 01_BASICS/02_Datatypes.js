6cd17b33c44922cdba529a0723cfb30bfba4e2e2
Saniya Aryan <aryansaniya7@gmail.com>
2025-11-07 14:03:56 +0000

    Datatypes in Js

---
+++ 01_BASICS/02_Datatypes.js
@@ -0,0 +1,13 @@
+let name = "saniya"
+let age = 18
+let isloggedin = true
+let state = null
+
+console.log(typeof age);
+console.log(typeof name);
+console.log(typeof isloggedin);
+console.log(typeof state);
+
+// Datattypes in js => Number,String,Boolean,Null,undefined,symbol,BigInt (Primitive Datatypes)
+
+// Array,Object,Function (Non-Primitive Datatypes)
