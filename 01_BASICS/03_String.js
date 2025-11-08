3e93cc49a5c5213d40a2888092a76825297defe2
Saniya Aryan <aryansaniya7@gmail.com>
2025-11-08 18:15:29 +0530

    Improve comment for toUpperCase method

--- 01_BASICS/03_String.js
+++ 01_BASICS/03_String.js
@@ -5,7 +5,7 @@ console.log(`my name is ${name} and my repocount is ${repocount}`);
 
 const anothername = new String('Aryan');
 console.log(anothername.length); // Shows the total number of characters in the string
-console.log(anothername.toUpperCase()); // Shows the total number of characters in the string
+console.log(anothername.toUpperCase()); // Convert all letters in the string to uppercase
 console.log(anothername.charAt(2));// Shows the character at index 2 (remember, counting starts from 0)
 console.log(anothername.indexOf('a'));// Finds the index of the first occurrence of 'a' in the string
 
