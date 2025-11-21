d622725ee6ce2037da7332290b41f672d34161f1
Saniya Aryan <aryansaniya7@gmail.com>
2025-11-21 13:03:41 +0000

    Add example of object.

---
+++ 02_BASICS/ObjQues4.js
@@ -0,0 +1,13 @@
+// Write the code for this object:
+
+// Create an object called book with properties
+    // title: "JavaScript Basics"
+    // author: "Saniya"
+    // year: 2025
+
+const book = new Object ();
+book.title = "JavaScript Basics";
+book.author = "saniya";
+book.year = 2025;
+
+console.log(book);
