c9cae01aed2bc18639bab3d4cc02b2733c6fa472
Saniya Aryan <aryansaniya7@gmail.com>
2025-11-22 18:15:16 +0000

    Implement object inside object (Nested object)

---
+++ 02_BASICS/12_Method2.js
@@ -0,0 +1,12 @@
+//              Object inside + Object Method
+// Object inside Object (Nested object)
+
+const user = {
+    name : "Saniya",
+    details : {
+        age : 21,
+        course : "BCA"
+    }
+};
+console.log(user.details.age);
+console.log(user.details.course);
