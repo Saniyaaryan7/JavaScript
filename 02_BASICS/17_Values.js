632ee849f56c9b7bbb3551f4e426d89a8f0346d8
Saniya Aryan <aryansaniya7@gmail.com>
2025-11-23 18:31:26 +0000

    Add a example of using Oject.Values()

---
+++ 02_BASICS/17_Values.js
@@ -0,0 +1,9 @@
+// Object Iteration : (Keys,Values,entries)
+// Oject.Values()
+
+const student = {
+    name : "Saniya",
+    course : "BCA",
+    year : 2025
+};
+console.log(Object.values(student));
