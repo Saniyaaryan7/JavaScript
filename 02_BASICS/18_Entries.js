78022b17ea795e4896378703b443e4bebb22507d
Saniya Aryan <aryansaniya7@gmail.com>
2025-11-23 18:33:54 +0000

    Add a example of using Oject.Entries()

---
+++ 02_BASICS/18_Entries.js
@@ -0,0 +1,9 @@
+// Object Iteration : (Keys,Values,entries)
+// Oject.Entries()
+
+const student = {
+    name : "Saniya",
+    course : "BCA",
+    year : 2025
+};
+console.log(Object.entries(student));
