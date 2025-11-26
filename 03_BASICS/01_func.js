e0d11a5d49ef193950cd5dddab5353f9d764266d
Saniya Aryan <aryansaniya7@gmail.com>
2025-11-26 18:34:13 +0000

    Add two number using function method

--- 03_BASICS/01_func.js
+++ 03_BASICS/01_func.js
@@ -1,4 +1,11 @@
-function addTwoNumber (number1,number2){
-    console.log(number1+number2);
+// function addTwoNumber (number1,number2){
+//     console.log(number1+number2);
+// }
+// addTwoNumber(3,2)
+
+function addTwoNumber(number1,number2){
+    let result = number1+number2
+    return result
 }
-addTwoNumber(3,2)
+const result = addTwoNumber(3,4)
+console.log("Result :",result);
