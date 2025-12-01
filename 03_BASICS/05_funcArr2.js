589dbf98bd028d55924316abca403a42cc3a3980
Saniya Aryan <aryansaniya7@gmail.com>
2025-12-01 13:35:52 +0000

    Add function to return element from array .

---
+++ 03_BASICS/05_funcArr2.js
@@ -0,0 +1,6 @@
+const mynewArr = [200,400,600,800]
+function returnSecondvalue(getArray){
+    return getArray[2]
+}
+console.log(returnSecondvalue(mynewArr));
+console.log(returnSecondvalue([400,800,1200,1600]));   // Passing array directly
