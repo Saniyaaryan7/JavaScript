e41f70ec59649286294eb4952ddd5dd99ef14b90
Saniya Aryan <aryansaniya7@gmail.com>
2025-12-03 17:46:35 +0000

    return interrupts function execution

---
+++ 03_BASICS/08_return.js
@@ -0,0 +1,10 @@
+function check(){
+    console.log("Start");  // This will run
+
+    return 100 ;         // Function returns here
+
+    console.log("End");  // This will NOT run 
+}
+
+console.log(check());
+
