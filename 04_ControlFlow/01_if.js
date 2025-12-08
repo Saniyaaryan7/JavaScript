ac4b16211cae8644fd4d5d4b60de412b1915e308
Saniya Aryan <aryansaniya7@gmail.com>
2025-12-08 17:22:55 +0000

    Implement simple if-else condition

---
+++ 04_ControlFlow/01_if.js
@@ -0,0 +1,10 @@
+// Store the current temperature
+const temperature = 41;
+
+// Check if temperature is greater than 45
+if (temperature > 45) {
+    console.log("Temperature is greater than 45 degree");   // Runs when temp > 45
+}
+else {
+    console.log("Less than 45 degree");  // Runs when temp < 45
+}
