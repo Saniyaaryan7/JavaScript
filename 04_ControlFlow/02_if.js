73181d611c5b9e9e47a03e7418ff0760dce279e4
Saniya Aryan <aryansaniya7@gmail.com>
2025-12-08 17:42:21 +0000

    if-else order

---
+++ 04_ControlFlow/02_if.js
@@ -0,0 +1,20 @@
+const score = 600
+if(score>100){
+    const exam ="Final"
+    console.log(`Score is greater than 600 in ${exam} examination`);
+}
+// ---------------------------------------------------------------------------
+
+const balance = 100
+if(balance>1500){
+    console.log("Your Balance is more than 1500");
+}
+else if(balance<500){
+console.log("Your Balance is under 500");
+}
+else if(balance<1500){
+console.log("Your Balance is under 1500");
+}
+else{
+    console.log("Your Account is Deactivated");
+}
