8ad920a31c241af464922a7e6dd421322ba06e54
Saniya Aryan <aryansaniya7@gmail.com>
2025-12-20 09:01:05 +0000

    Add ternary condition example

---
+++ 04_ControlFlow/05_Terniary.js
@@ -0,0 +1,7 @@
+const iceteaprice = 100
+// Ternary operator to check price condition
+// If price >= 80 -> print "more than 80"
+// Else -> print "Less than 80"
+iceteaprice>=80 
+? console.log("more tha 80")
+:console.log("Less tha 80")
