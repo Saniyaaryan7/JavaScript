d9ca098a636fd4bd4fcd7b42d82d63781013d887
Saniya Aryan <aryansaniya7@gmail.com>
2025-12-22 09:24:30 +0000

    Add examples of while loop

---
+++ 05_LOOP/04_while.js
@@ -0,0 +1,12 @@
+let index = 0
+while(index<=10){
+    console.log(`Value of index is ${index}`);
+    index = index+2
+}
+
+let myArr = ['Flash','Spiderman','Superman']
+let arr=0
+while(arr<myArr.length){
+    console.log(`Value of array is ${myArr[arr]}`);
+    arr=arr+1
+}
