1ccceb26e75e74fcab5fe370762b815d8ef1fe4e
Saniya Aryan <aryansaniya7@gmail.com>
2025-12-22 09:38:01 +0000

    Add for-of loop examples for arrays and strings

---
+++ 06_High_Order_Array_loop/01_forOf.js
@@ -0,0 +1,13 @@
+// Array of numbers
+const arr = [1,2,3,4,5]
+
+for(const i of arr){
+    console.log(i);
+}
+
+// String value
+const greetings = "Helloworld"
+// for...of loop to iterate over each character in the string
+for(const greet of greetings){
+    console.log(`Each character is ${greet}`);
+}
